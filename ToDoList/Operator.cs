fab08e6873382b27bd5ec3ed0dd5a2ebf7aa718e
Kauan <Kauan@DESKTOP-UEKMN1L>
2023-05-01 17:06:15 -0300

    Add project files.

---
+++ ToDoList/Operator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    class Operator
+    {
+        string[] toDo = new string[10];
+
+        public string List()
+        {
+            Console.WriteLine("\n----------------------------------------------------------------------------\n");
+            Console.WriteLine("CHOOSE A LINE! ( 1 - 10 )");
+
+            Console.WriteLine("\n----------------------------------------------------------------------------\n");
+            int line = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("INPUT ACTIVITY BELOW: ");
+            string activity = Console.ReadLine();
+            if (activity != null)
+            {
+                toDo[line] = activity;
+            }else
+            {
+                Console.WriteLine("INPUT VALID ACTIVITY BELOW: ");
+                activity = Console.ReadLine();
+                if (activity != null)
+                {
+                    toDo[line] = activity;
+                }
+                
+            }
+
+            return "";
+        }
+        public string OutList()
+        {
+            return $"1:{toDo[0]}\n2: {toDo[1]}\n3: {toDo[2]}\n4: {toDo[3]}\n5: {toDo[4]}\n6:{toDo[5]}\n7: {toDo[6]}\n8: {toDo[7]}\n9: {toDo[8]}\n10: {toDo[9]}";
+        }
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.Design;
+using System.Globalization;
+using ToDoList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int menu;
+        int confirm;
+        try
+        { 
+            Operator Operator = new Operator();
+            do
+            {
+                Console.WriteLine("\n----------------------------------------------------------------------------\n");
+                Console.WriteLine("!TO-DO LIST!\n");
+                Console.WriteLine("1 - MODIFY LIST\n2 - FINISH PROGRAM\n");
+                Console.WriteLine("----------------------------------------------------------------------------\n");
+                Console.WriteLine(Operator.OutList());
+                Console.WriteLine("\n----------------------------------------------------------------------------\n");
+                menu = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (menu == 1)
+                {
+                    Console.WriteLine(Operator.List());
+                    Console.Clear();
+                }
+                else if (menu == 2)
+                {
+                    do
+                    {
+                        Console.WriteLine("\n----------------------------------------------------------------------------\n");
+                        Console.WriteLine("TYPE 2 TO CONFIRM FINISH SYSTEM.\nTYPE 1 TO RETURN.");
+                        Console.WriteLine("\n----------------------------------------------------------------------------\n");
+                        confirm = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                        if (confirm == 2)
+                        {
+                            menu = 834834;
+                        }
+                        else if (confirm == 1) { }
+                    } while (confirm != 1 && confirm != 2);
+                }
+            } while (menu != 834834);
+
+            Console.WriteLine("\nFINISHED.\n");
+            Console.WriteLine("----------------------------------------------------------------------------\n");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
